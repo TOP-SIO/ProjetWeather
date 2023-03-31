5d273d2cd08a49b50f94d5941120432cb6e50355
Gaëtan <98382841+Zigatar1@users.noreply.github.com>
2023-03-31 12:33:14 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import WeatherInfo from './components/WeatherInfo';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <WeatherInfo />
+    </View>
+  );
+}
