4ae96bd552ea80836239ab4f0634e180e9dd18f0
Gaëtan <98382841+gaesty@users.noreply.github.com>
2023-04-07 14:44:19 +0200

    Add files via upload

---
+++ projet/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import WeatherInfo from './components/WeatherInfo';
+import ChambreInfo from './components/ChambreInfo';
+
+export default function App() {
+  return (
+    <View style={[styles.container, { backgroundColor: '#301F3C' }]}>
+      <WeatherInfo />
+      <ChambreInfo />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent : 'space-around',
+    maxWidth: 800,
+    maxHeight: 400,
+  }
+});
