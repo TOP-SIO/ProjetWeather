5fe99d6896ad4d441dc22cdca7f541d3af0bee87
ThomasSengatKuo <108008731+ThomasSengatKuo@users.noreply.github.com>
2023-05-29 18:23:48 +0200

    login page updated

--- projet/components/Login.js
+++ projet/components/Login.js
@@ -1,141 +1,100 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export const Login = () => {
   return (
     <View style={styles.loginWrapper}>
       <View style={styles.login}>
-        <View style={styles.loginOverlapGroup}>
-          <Text style={styles.authentication}>Authentification</Text>
-          <View style={styles.zoneIdentifiant}>
-            <Text style={styles.usernameLabel}>Identifiant</Text>
-          </View>
-          <View style={styles.zoneMDP}>
-            <Text style={styles.passwordLabel}>Mot de passe</Text>
-          </View>
-          <View style={styles.loginOverlapGroup1}>
-            <View style={styles.rectangle4} />
-            <Text style={styles.connect}>Se connecter</Text>
-          </View>
-          <Text style={styles.passwordForgotten}>Mot de passe oublié</Text>
-        </View>
-        {/* 
         <Image
-          style={styles.backgroundImage}
-          source={require('./45332c6a-e332-4225-80ac-c6fd7047d444-removebg-preview-1.png')}
+          source={require('../assets/topsio.png')}
+          style={styles.logo}
         />
-        */}
-        <View style={styles.rectangle3} />
+        <Text style={styles.authentication}>Authentification</Text>
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Entrez votre identifiant"
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Entrez votre mot de passe"
+            secureTextEntry={true}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Se connecter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.forgotPasswordText}>Mot de passe oublié</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+
+    logo: {
+    height: '20%', 
+    resizeMode: 'contain', 
+    marginBottom: '5%',
+  },
+
   loginWrapper: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#301F3C',
+    padding: '5%',
   },
   login: {
-    width: 1440,
-    height: 960,
-  },
-  loginOverlapGroup: {
-    position: 'absolute',
-    left: 318,
-    top: 371,
-    width: 804,
-    height: 477,
+    width: '100%',
+    height: '100%',
     backgroundColor: '#513465',
     borderRadius: 41,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '5%',
   },
   authentication: {
-    position: 'absolute',
-    left: 275,
-    top: 28,
     fontSize: 36,
     lineHeight: 41,
     color: '#FFFFFF',
     fontFamily: 'Ubuntu',
+    marginBottom: '5%',
   },
-  zoneIdentifiant: {
-    position: 'absolute',
-    left: 107,
-    top: 137,
-    width: 604,
-    height: 71,
+  inputWrapper: {
+    width: '100%',
+    marginBottom: '5%',
+  },
+  textInput: {
+    width: '100%',
+    height: 50,
     backgroundColor: '#FDFBF7',
     borderRadius: 25,
+    fontSize: 20,
+    fontFamily: 'Ubuntu',
+    paddingHorizontal: '5%',
+    marginBottom: '5%',
   },
-  usernameLabel: {
-    position: 'absolute',
-    left: 89,
-    top: 18,
+  button: {
+    width: '60%',
+    height: 50,
+    backgroundColor: '#FDFBF7',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '5%',
+  },
+  buttonText: {
     fontSize: 24,
     lineHeight: 28,
     color: '#000000',
     fontFamily: 'Ubuntu',
   },
-  zoneMDP: {
-    position: 'absolute',
-    left: 107,
-    top: 218,
-    width: 604,
-    height: 71,
-    backgroundColor: '#FDFBF7',
-    borderRadius: 25,
-  },
-  passwordLabel: {
-    position: 'absolute',
-    left: 89,
-    top: 18,
-    fontSize: 24,
-    lineHeight: 28,
-    color: '#000000',
-    fontFamily: 'Ubuntu',
-  },
-  loginOverlapGroup1: {
-    position: 'absolute',
-    left: 274,
-    top: 343,
-    width: 255,
-    height: 79,
-    backgroundColor: '#FDFBF7',
-    borderRadius: 25,
-  },
-    connect: {
-    position: 'absolute',
-    left: 45,
-    top: 16,
-    fontSize: 24,
-    lineHeight: 28,
-    color: '#000000',
-    fontFamily: 'Ubuntu',
-  },
-  passwordForgotten: {
-    position: 'absolute',
-    left: 311,
-    top: 308,
+  forgotPasswordText: {
     fontSize: 20,
     lineHeight: 23,
     color: '#FFFFFF',
     fontFamily: 'Ubuntu',
-  },
-  backgroundImage: {
-    position: 'absolute',
-    left: 214,
-    top: 63,
-    width: 376,
-    height: 213,
-  },
-  rectangle3: {
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    width: 1440,
-    height: 165,
-    backgroundColor: '#513465',
   },
 });
